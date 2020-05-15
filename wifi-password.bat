981c7cd9e7b2c04590384871b0acb6038110dc88
SachaDee <sachadehe@gmail.com>
2020-05-15 18:05:07 -0300

    Add files via upload

---
+++ wifi-password.bat
@@ -0,0 +1,3 @@
+:sachaDee 2020
+
+@for /f "skip=2 delims=: tokens=2" %%a in ('netsh wlan show profiles') do @for /f "tokens=2 delims=:" %%b in ('netsh wlan show profile name^=%%a key^=clear ^| findstr "Cont"') do @echo %%a =^> %%b
